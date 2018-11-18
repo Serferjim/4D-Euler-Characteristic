580a4040616f5889392b6fc5c23aa4c88cdc5ef4
Serferjim <39151400+Serferjim@users.noreply.github.com>
2018-11-18 12:48:48 +0100

    Cambios en llamada
    Eliminados los parametros de entrada de los conjuntos de celdas y el dinamismo respecto a las dimensiones

--- eulerPoincareCharacteristic3D.m
+++ eulerPoincareCharacteristic3D.m
@@ -1,26 +1,20 @@
-function [eulerCharacteristic] = eulerPoincareCharacteristic3D(inputImage, threshold,zeroCells,oneCells, twoCells, threeCells)
+function [eulerCharacteristic] = eulerPoincareCharacteristic3D(inputImage, threshold)
 %EULERPOINCARECHARACTERISTIC4D Summary of this function goes here
 %   Detailed explanation goes here
 poolobj = gcp;
-imageToAnalize = inputImage;
+imageToAnalize = nrrdread(inputImage);
 dimensions = size(imageToAnalize);
 %for w=1: size(imageToAnalize,1)
-n0 = zeroCells;
-n1 = oneCells;
-n2 = twoCells;
-n3 = threeCells;
+n0 = {};
+n1 = {};
+n2 = {};
+n3 = {};
 for x=1: size(imageToAnalize,1)
-    if size(dimensions) >= 3
-        eulerCharacteristic = eulerPoincareCharacteristic3D(imageToAnalize(x,:,:), threshold, n0, n1, n2, n3);
-    else
         [n0,n1,n2,n3] = cellAnalizer(imageToAnalize(x,:,:),x, threshold,n0,n1,n2,n3);
-        
-    end
 end
 
 
 %end
-
 eulerCharacteristic = size(n0,2) -size(n1,2) +size(n2,2) - size(n3,2);
 delete(poolobj);
 
