43925c213942d64cabcebfb3b3b1a3e2730161e9
Serferjim <39151400+Serferjim@users.noreply.github.com>
2018-11-29 19:17:27 +0100

    Cambios gordos
    Se ha vuelto a implementar el algoritmo de forma iterativa lineal.
    
    Se han eliminado los archivos core y cellAnalizer ya que no son necesarios

--- eulerPoincareCharacteristic3D.m
+++ eulerPoincareCharacteristic3D.m
@@ -1,7 +1,6 @@
 function [eulerCharacteristic] = eulerPoincareCharacteristic3D(inputImage, threshold)
 %EULERPOINCARECHARACTERISTIC4D Summary of this function goes here
 %   Detailed explanation goes here
-poolobj = gcp;
 imageToAnalize = nrrdread(inputImage);
 dimensions = size(imageToAnalize);
 %for w=1: size(imageToAnalize,1)
@@ -10,13 +9,98 @@ n1 = {};
 n2 = {};
 n3 = {};
 for x=1: size(imageToAnalize,1)
-        [n0,n1,n2,n3] = cellAnalizer(imageToAnalize(x,:,:),x, threshold,n0,n1,n2,n3);
-end
-
-
-%end
-eulerCharacteristic = size(n0,2) -size(n1,2) +size(n2,2) - size(n3,2);
-delete(poolobj);
-
-end
-
+    for y=1: size(subImage,2)
+        for z=1: size(subImage,3)
+            if subImage(1,y,z) >= threshold
+                if  not(ismember(num2str([coordPrime-0.5,y-0.5,z-0.5]), n0))
+                    n0{end +1} = num2str([coordPrime-0.5,y-0.5,z-0.5]);
+                end
+                if  not(ismember(num2str([coordPrime-0.5,y-0.5,z+0.5]), n0))
+                    n0{end +1} = num2str([coordPrime-0.5,y-0.5,z+0.5]);
+                end
+                if  not(ismember(num2str([coordPrime-0.5,y+0.5,z-0.5]), n0))
+                    n0{end +1} = num2str([coordPrime-0.5,y+0.5,z-0.5]);
+                end
+                if  not(ismember(num2str([coordPrime-0.5,y+0.5,z+0.5]), n0))
+                    n0{end+1} = num2str([coordPrime-0.5,y+0.5,z+0.5]);
+                end
+                if  not(ismember(num2str([coordPrime+0.5,y-0.5,z-0.5]), n0))
+                    n0{end+1} = num2str([coordPrime+0.5,y-0.5,z-0.5]);
+                end
+                if  not(ismember(num2str([coordPrime+0.5,y-0.5,z+0.5]), n0))
+                    n0{end+1} = num2str([coordPrime+0.5,y-0.5,z+0.5]);
+                end
+                if  not(ismember(num2str([coordPrime+0.5,y+0.5,z-0.5]), n0))
+                    n0{end+1} = num2str([coordPrime+0.5,y+0.5,z-0.5]);
+                end
+                if  not(ismember(num2str([coordPrime+0.5,y+0.5,z+0.5]), n0))
+                    n0{end+1} = num2str([coordPrime+0.5,y+0.5,z+0.5]);
+                end
+                %1-Celdas
+                if not(ismember(num2str([coordPrime,y-0.5,z-0.5]),n1))
+                    n1{end+1} = num2str([coordPrime,y-0.5,z-0.5]);
+                end
+                if not(ismember(num2str([coordPrime,y-0.5,z+0.5]),n1))
+                    n1{end+1} = num2str([coordPrime,y-0.5,z+0.5]);
+                end
+                if not(ismember(num2str([coordPrime,y+0.5,z-0.5]),n1))
+                    n1{end+1} = num2str([coordPrime,y+0.5,z-0.5]);
+                end
+                if not(ismember(num2str([coordPrime,y+0.5,z+0.5]),n1))
+                    n1{end+1} = num2str([coordPrime,y+0.5,z+0.5]);
+                end
+                if not(ismember(num2str([coordPrime-0.5,y,z-0.5]),n1))
+                    n1{end+1} = num2str([coordPrime-0.5,y,z-0.5]);
+                end
+                if not(ismember(num2str([coordPrime-0.5,y,z+0.5]),n1))
+                    n1{end+1} = num2str([coordPrime-0.5,y,z+0.5]);
+                end
+                if not(ismember(num2str([coordPrime+0.5,y,z-0.5]),n1))
+                    n1{end+1} = num2str([coordPrime+0.5,y,z-0.5]);
+                end
+                if not(ismember(num2str([coordPrime+0.5,y,z+0.5]),n1))
+                    n1{end+1} = num2str([coordPrime+0.5,y,z+0.5]);
+                end
+                if not(ismember(num2str([coordPrime-0.5,y-0.5,z]),n1))
+                    n1{end+1} = num2str([coordPrime-0.5,y-0.5,z]);
+                end
+                if not(ismember(num2str([coordPrime-0.5,y+0.5,z]),n1))
+                    n1{end+1} = num2str([coordPrime-0.5,y+0.5,z]);
+                end
+                if not(ismember(num2str([coordPrime+0.5,y-0.5,z]),n1))
+                    n1{end+1} = num2str([coordPrime+0.5,y-0.5,z]);
+                end
+                if not(ismember(num2str([coordPrime+0.5,y+0.5,z]),n1))
+                    n1{end+1} = num2str([coordPrime+0.5,y+0.5,z]);
+                end
+                %2-Celdas
+                if not(ismember(num2str([coordPrime,y,z-0.5]),n2))
+                    n2{end+1} = num2str([coordPrime,y,z-0.5]);
+                end
+                if not(ismember(num2str([coordPrime,y,z+0.5]),n2))
+                    n2{end+1} = num2str([coordPrime,y,z+0.5]);
+                end
+                if not(ismember(num2str([coordPrime,y-0.5,z]),n2))
+                    n2{end+1} = num2str([coordPrime,y-0.5,z]);
+                end
+                if not(ismember(num2str([coordPrime,y+0.5,z]),n2))
+                    n2{end+1} = num2str([coordPrime,y+0.5,z]);
+                end
+                if not(ismember(num2str([coordPrime-0.5,y,z]),n2))
+                    n2{end+1} = num2str([coordPrime-0.5,y,z]);
+                end
+                if not(ismember(num2str([coordPrime+0.5,y,z]),n2))
+                    n2{end+1} = num2str([coordPrime+0.5,y,z]);
+                end
+                %3-Celdas
+                n3{end+1} = [coordPrime,y,z];
+                
+            end
+            
+        end
+        
+        %end
+        eulerCharacteristic = size(n0,2) -size(n1,2) +size(n2,2) - size(n3,2);
+        
+    end
+    
